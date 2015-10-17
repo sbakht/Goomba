0a4d3341ab180c2dd51dab3a1c4e8f29c3c2a2ba
Saad Bakht <rotk13@gmail.com>
2015-10-17 13:55:42 -0400

    Game type filter checklist

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -17,17 +17,19 @@ angular.module('goombaApp')
 
 	this.allLevels = ObjectFactory.createLevelCollection();
 	var level = ObjectFactory.createLevel({title: "Mario Land", ID:"123-456", game: "Super Mario World", difficulty: "Medium", tags: tags});
-	var level2 = ObjectFactory.createLevel({title: "Yoshi World", ID:"456-433", game: "Supe Mario Bros", difficulty: "Easy", tags: tags2});
-	var level3 = ObjectFactory.createLevel({title: "Molly's Mushers", ID:"123-456", game: "Super Mario World", difficulty: "Medium"});
-	var level4 = ObjectFactory.createLevel({title: "Impossible Feat", ID:"343-522", game: "Super Mario Wii", difficulty: "Hard"});
+	var level2 = ObjectFactory.createLevel({title: "Yoshi World", ID:"456-433", game: "Super Mario Bros.", difficulty: "Easy", tags: tags2});
+	var level3 = ObjectFactory.createLevel({title: "Molly's Mushers", ID:"123-456", game: "Super Mario Bros. 3", difficulty: "Medium"});
+	var level4 = ObjectFactory.createLevel({title: "Impossible Feat", ID:"343-522", game: "New Super Mario Bros. U", difficulty: "Hard"});
 	this.allLevels.add(level);
 	this.allLevels.add(level2);
 	this.allLevels.add(level3);
 	this.allLevels.add(level4);
 	level2.options.title = "Changing title from options.title";
 	level2.addTag(playsItselfTag);
-	this.filters = {difficulty: []};
 	this.difficulty = {};
+	this.game = {};
+	this.filters = {game: [], difficulty: []};
+
 	this.updateDifficultyFilter = function() {
 		this.filters.difficulty = [];
 		if(this.difficulty.easy) {
@@ -39,7 +41,7 @@ angular.module('goombaApp')
 		if(this.difficulty.hard) {
 			this.filters.difficulty.push("Hard");
 		}
-		this.filtered = this.allLevels.filter({difficulty: this.filters.difficulty});
+		this.filtered = this.allLevels.filter(this.filters);
 	};
 	this.selectAllDifficulty = function() {
 		this.difficulty.easy = true;
@@ -47,5 +49,29 @@ angular.module('goombaApp')
 		this.difficulty.hard = true;
 		this.updateDifficultyFilter();
 	};
+	this.updateGameFilter = function() {
+		this.filters.game = [];
+		if(this.game.smb) {
+			this.filters.game.push("Super Mario Bros.");
+		}
+		if(this.game.smb3) {
+			this.filters.game.push("Super Mario Bros. 3");
+		}
+		if(this.game.smw) {
+			this.filters.game.push("Super Mario World");
+		}
+		if(this.game.smbu) {
+			this.filters.game.push("New Super Mario Bros. U");
+		}
+		this.filtered = this.allLevels.filter(this.filters);
+	};
+	this.selectAllGame = function() {
+		this.game.smb = true;
+		this.game.smb3 = true;
+		this.game.smw = true;
+		this.game.smbu = true;
+		this.updateGameFilter();
+	};
 	this.selectAllDifficulty();
+	this.selectAllGame();
   });
